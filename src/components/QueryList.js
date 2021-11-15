b8504b8f68df84804ccd6997c0194732ab53a28b
Arttu <arttu.ojanen1@gmail.com>
2021-11-15 13:50:12 +0200

    Eka frontti

---
+++ src/components/QueryList.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+function QueryList() {
+    const [question, setQuestion] = useState([]);
+
+    const express = require("express");
+    const app = express();
+    const cors = require("cors");
+
+    app.use(
+        cors({
+            origin: "*",
+        })
+    )
+    app.get('/', function (req, res) {
+        res.send('Hello World')
+    })
+    app.listen(3000);
+
+
+    useEffect(() => {
+        // Fetch questions
+       fetchQuestions();
+    }, []);
+
+    const fetchQuestions = () => {
+        fetch('http://localhost:8080/test',
+        {
+        method: 'GET',
+        headers: {'Content-type':'application/json'},
+        }
+        )
+        .then(response => response.json())
+        .then(data => setQuestion(data._embedded.question))
+        .catch(err => console.error(err))
+    }
+
+
+    return(
+        <div>
+
+            <p>aaaa{question}</p>
+        </div>
+    );
+}
+
+export default QueryList;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import QueryList from './components/QueryList';
 
 ReactDOM.render(
   <React.StrictMode>
