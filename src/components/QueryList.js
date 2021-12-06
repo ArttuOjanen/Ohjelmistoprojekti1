6a84f6f5db69447b012e3c7529bbbd0dc0967431
ArttuOjanen <arttu.ojanen1@gmail.com>
2021-12-06 10:52:34 +0200

    Front-end

--- src/components/QueryList.js
+++ src/components/QueryList.js
@@ -1,46 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Express } from 'express';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
 
 function QueryList() {
+    
     const [question, setQuestion] = useState([]);
-
-    const express = require("express");
-    const app = express();
-    const cors = require("cors");
-
-    app.use(
-        cors({
-            origin: "*",
-        })
-    )
-    app.get('/', function (req, res) {
-        res.send('Hello World')
-    })
-    app.listen(3000);
+    const url = 'http://localhost:8080/api/queries';
 
 
     useEffect(() => {
-        // Fetch questions
        fetchQuestions();
     }, []);
 
     const fetchQuestions = () => {
-        fetch('http://localhost:8080/test',
-        {
-        method: 'GET',
-        headers: {'Content-type':'application/json'},
-        }
-        )
+        fetch(url, {
+          mode: 'no-cors',
+          referrerPolicy: 'no-referrer'
+        })
         .then(response => response.json())
-        .then(data => setQuestion(data._embedded.question))
+        .then(data => setQuestion(data._embedded.queries))
         .catch(err => console.error(err))
     }
+  
+        
 
 
     return(
         <div>
-
-            <p>aaaa{question}</p>
+        <div>
+       
+<  FormControl component="fieldset">
+  <FormLabel component="legend">{question}</FormLabel>
+  <RadioGroup
+    defaultValue="kyllä"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="kyllä" control={<Radio />} label="Kyllä" />
+    <FormControlLabel value="ei" control={<Radio />} label="ei" />
+  </RadioGroup>
+</FormControl>
+        
+        </div>
+        <Button variant="contained">Lähetä</Button>
+        {}
         </div>
     );
 }
