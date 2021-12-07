045868d36f1364ea7001bdc64a41557709ca2979
ArttuOjanen <arttu.ojanen1@gmail.com>
2021-12-07 14:42:03 +0200

    CORS defeated

--- src/components/QueryList.js
+++ src/components/QueryList.js
@@ -1,10 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Button from '@mui/material/Button';
 
 function QueryList() {
     
@@ -12,41 +6,25 @@ function QueryList() {
     const url = 'http://localhost:8080/api/queries';
 
 
-    useEffect(() => {
-       fetchQuestions();
-    }, []);
-
     const fetchQuestions = () => {
-        fetch(url, {
-          mode: 'no-cors',
-          referrerPolicy: 'no-referrer'
-        })
-        .then(response => response.json())
-        .then(data => setQuestion(data._embedded.queries))
-        .catch(err => console.error(err))
-    }
-  
-        
+      fetch('http://localhost:8080/api/queries')
+      .then(response => response.json())
+      .then(data => setQuestion(data._embedded.queries))
+      .catch(err => console.error(err))
+  }
 
+    useEffect(() => {
+      fetchQuestions();
+   }, []);
+  
 
     return(
         <div>
-        <div>
-       
-<  FormControl component="fieldset">
-  <FormLabel component="legend">{question}</FormLabel>
-  <RadioGroup
-    defaultValue="kyllä"
-    name="radio-buttons-group"
-  >
-    <FormControlLabel value="kyllä" control={<Radio />} label="Kyllä" />
-    <FormControlLabel value="ei" control={<Radio />} label="ei" />
-  </RadioGroup>
-</FormControl>
-        
-        </div>
-        <Button variant="contained">Lähetä</Button>
-        {}
+       { question.map((query) =>
+       <tr key={query}>
+         <td>{query.question}</td>
+       </tr>
+       )}
         </div>
     );
 }
